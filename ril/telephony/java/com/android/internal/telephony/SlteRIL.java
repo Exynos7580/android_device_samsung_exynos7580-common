f7040e5936dc66f0ab298aff8afb2aa384ab11d1
Apavayan <info@apavayan.com>
2016-05-24 11:07:48 +0530

    SlteRIL: fix incoming calls
    Change-Id: I01994ebbc8d6c733c8278a91b001e08a55d28735
    Signed-off-by: Apavayan <info@apavayan.com>

--- ril/telephony/java/com/android/internal/telephony/SlteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/SlteRIL.java
@@ -193,10 +193,10 @@ public class SlteRIL extends RIL {
             dc.als = p.readInt();
             dc.isVoice = (0 != p.readInt());
 
-            boolean isVideo = (0 != p.readInt());   // Samsung
-            int call_type = p.readInt();            // Samsung CallDetails
-            int call_domain = p.readInt();          // Samsung CallDetails
-            String csv = p.readString();            // Samsung CallDetails
+            //boolean isVideo = (0 != p.readInt());   // Samsung
+            //int call_type = p.readInt();            // Samsung CallDetails
+            //int call_domain = p.readInt();          // Samsung CallDetails
+            //String csv = p.readString();            // Samsung CallDetails
 
             dc.isVoicePrivacy = (0 != p.readInt());
             dc.number = p.readString();
