fdf12f9af8f109a42832141405593022f031165a
Christopher N. Hesse <raymanfx@gmail.com>
2016-10-03 15:50:09 +0200

    exynos7580: audio: Fix the BT handsfree fix
    Change-Id: I1b3cf8a6b232acbf50e3fdfc6f94a07cd3ef2e80

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -1169,7 +1169,7 @@ static void do_out_standby(struct stream_out *out)
     /* if in-call, dont turn off PCM */
     if (adev->in_call) {
         ALOGV("%s: output standby in-call, exiting...", __func__);
-        return 0;
+        return;
     }
 
     if (!out->standby) {
@@ -1553,7 +1553,7 @@ static void do_in_standby(struct stream_in *in)
     /* if in-call, dont turn off PCM */
     if (adev->in_call) {
         ALOGV("%s: input standby in-call, exiting...", __func__);
-        return 0;
+        return;
     }
 
     if (!in->standby) {
