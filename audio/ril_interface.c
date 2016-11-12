dbb5efeeac45fba0e515a1c29d4ecb54e845c0b1
Andreas Schneider <asn@cryptomilk.org>
2016-11-12 20:43:08 +0100

    audio: Better handling of ril->client
    Change-Id: Id767d2bc0a82051d5e915fc58d9f955e6b7c6314

--- audio/ril_interface.c
+++ audio/ril_interface.c
@@ -62,6 +62,11 @@ static int ril_connect_if_required(struct ril_handle *ril)
     int ok;
     int rc;
 
+    if (ril->client == NULL) {
+        ALOGE("ril->client is NULL");
+        return -1;
+    }
+
     ok = isConnected_RILD(ril->client);
     if (ok) {
         return 0;
@@ -128,6 +133,7 @@ int ril_close(struct ril_handle *ril)
         ALOGE("CloseClient_RILD() failed");
         return -1;
     }
+    ril->client = NULL;
 
     return 0;
 }
