afe6fde20bac0f2916048f3dcc9143d7e84dcf82
Christopher N. Hesse <raymanfx@gmail.com>
2017-01-08 00:03:23 +0100

    audio: improve read/write timing for streams
    Change-Id: Ic4231c4b44369a9438c6c94622b90862047e9a32
    Reference: https://github.com/LineageOS/android_hardware_libhardware/commit/0caeee8ac429dd8098e97b2cd8ad3751031a5b67

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -5,7 +5,7 @@
  *               Daniel Hillenbrand <codeworkx@cyanogenmod.com>
  *               Guillaume "XpLoDWilD" Lesniak <xplodgui@gmail.com>
  * Copyright (c) 2015-2016 Andreas Schneider <asn@cryptomilk.org>
- * Copyright (c) 2015-2016 Christopher N. Hesse <raymanfx@gmail.org>
+ * Copyright (c) 2015-2017 Christopher N. Hesse <raymanfx@gmail.org>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -242,6 +242,8 @@ struct stream_out {
     uint64_t written; /* total frames written, not cleared when entering standby */
 
     struct audio_device *dev;
+
+    int64_t last_write_time_us;
 };
 
 struct stream_in {
@@ -271,6 +273,8 @@ struct stream_in {
     struct pcm_config *config;
 
     struct audio_device *dev;
+
+    int64_t last_read_time_us;
 };
 
 #define STRING_TO_ENUM(string) { #string, string }
@@ -1234,6 +1238,8 @@ static int out_standby(struct audio_stream *stream)
 
     unlock_all_outputs(adev, NULL);
 
+    // out->last_write_time_us = 0; unnecessary as a stale write time has same effect
+
     return 0;
 }
 
@@ -1437,8 +1443,28 @@ exit:
 final_exit:
 
     if (ret != 0) {
-        usleep(bytes * 1000000 / audio_stream_out_frame_size(stream) /
-               out_get_sample_rate(&stream->common));
+        struct timespec t = { .tv_sec = 0, .tv_nsec = 0 };
+        clock_gettime(CLOCK_MONOTONIC, &t);
+        const int64_t now = (t.tv_sec * 1000000000LL + t.tv_nsec) / 1000;
+        const int64_t elapsed_time_since_last_write = now - out->last_write_time_us;
+        int64_t sleep_time = bytes * 1000000LL / audio_stream_out_frame_size(stream) /
+                   out_get_sample_rate(&stream->common) - elapsed_time_since_last_write;
+        if (sleep_time > 0) {
+            usleep(sleep_time);
+        } else {
+            // we don't sleep when we exit standby (this is typical for a real alsa buffer).
+            sleep_time = 0;
+        }
+        out->last_write_time_us = now + sleep_time;
+        // last_write_time_us is an approximation of when the (simulated) alsa
+        // buffer is believed completely full. The usleep above waits for more space
+        // in the buffer, but by the end of the sleep the buffer is considered
+        // topped-off.
+        //
+        // On the subsequent out_write(), we measure the elapsed time spent in
+        // the mixer. This is subtracted from the sleep estimate based on frames,
+        // thereby accounting for drain in the alsa buffer during mixing.
+        // This is a crude approximation; we don't handle underruns precisely.
     }
 
     return bytes;
@@ -1581,6 +1607,8 @@ static int in_standby(struct audio_stream *stream)
 
     pthread_mutex_unlock(&in->dev->lock);
     pthread_mutex_unlock(&in->lock);
+
+    in->last_read_time_us = 0;
 
     return 0;
 }
@@ -1725,9 +1753,30 @@ static ssize_t in_read(struct audio_stream_in *stream, void* buffer,
         memset(buffer, 0, bytes);
 
 exit:
-    if (ret < 0)
-        usleep(bytes * 1000000 / audio_stream_in_frame_size(stream) /
-               in_get_sample_rate(&stream->common));
+    if (ret < 0) {
+        struct timespec t = { .tv_sec = 0, .tv_nsec = 0 };
+        clock_gettime(CLOCK_MONOTONIC, &t);
+        const int64_t now = (t.tv_sec * 1000000000LL + t.tv_nsec) / 1000;
+
+        // we do a full sleep when exiting standby.
+        const bool standby = in->last_read_time_us == 0;
+        const int64_t elapsed_time_since_last_read = standby ?
+                0 : now - in->last_read_time_us;
+        int64_t sleep_time = bytes * 1000000LL / audio_stream_in_frame_size(stream) /
+                in_get_sample_rate(&stream->common) - elapsed_time_since_last_read;
+        if (sleep_time > 0) {
+            usleep(sleep_time);
+        } else {
+            sleep_time = 0;
+        }
+        in->last_read_time_us = now + sleep_time;
+        // last_read_time_us is an approximation of when the (simulated) alsa
+        // buffer is drained by the read, and is empty.
+        //
+        // On the subsequent in_read(), we measure the elapsed time spent in
+        // the recording thread. This is subtracted from the sleep estimate based on frames,
+        // thereby accounting for fill in the alsa buffer during the interim.
+    }
 
     pthread_mutex_unlock(&in->lock);
     return bytes;
