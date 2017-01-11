b75054466d34f55885e300d5b18b54ee10306b13
Apavayan Sinha <info@apavayan.com>
2017-01-11 08:52:20 +0530

    exynos7580: Blob out custom audio hal (build it from device tree)
    Change-Id: Ifb426053a16e7942c7c6e9cb8d4e82092102265a

--- device-common.mk
+++ device-common.mk
@@ -44,7 +44,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf
 
 PRODUCT_PACKAGES += \
-    audio.primary.universal7580 \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
