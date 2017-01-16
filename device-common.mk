b4876072858e74aa733bc965dbef13f167d7122a
Apavayan Sinha <info@apavayan.com>
2017-01-16 19:52:54 +0530

    exynos7580: Configure the interactive governor
    Change-Id: I3da5efc4a827f2c422dd25863376728ba109cc24

--- device-common.mk
+++ device-common.mk
@@ -70,6 +70,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.power.rc
+
 # Wifi
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
