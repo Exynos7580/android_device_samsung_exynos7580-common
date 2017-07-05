8d6263a52bb5a1a6f6cc923b847f9b4688f251ab
Andreas Schneider <asn@cryptomilk.org>
2017-07-05 19:46:02 +0000

    audio: Add a ALOGT logging function
    Used patch: https://git.cryptomilk.org/projects/android/android_device_samsung_sltexx.git/commit/hal/audio?h=AUDIOHW&id=0444ab8dea18155b2741a42495d555491b4c1e71

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -22,6 +22,7 @@
 
 #define LOG_TAG "audio_hw_primary"
 #define LOG_NDEBUG 0
+//#define ALOG_TRACE 1
 
 #include <errno.h>
 #include <pthread.h>
@@ -49,6 +50,12 @@
 
 #include "routing.h"
 #include "ril_interface.h"
+
+#ifdef ALOG_TRACE
+#define ALOGT ALOGV
+#else
+#define ALOGT(a...) do { } while(0)
+#endif
 
 #define PCM_CARD 0
 #define PCM_CARD_SPDIF 1
