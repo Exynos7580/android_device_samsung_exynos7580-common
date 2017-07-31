814e2dbd5922179427b492191ce59a8f744f638f
Paul Keith <javelinanddart@gmail.com>
2017-07-31 11:14:44 +0000

    Add vsync phase offset
    * Kanged values from hammerhead
    * Reference:
      http://stackoverflow.com/questions/27947848/understanding-necessity-of-android-vsync-signals
    
    Change-Id: I7c6e7998b4c0d7b593c9d36318624cdc506aac74
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    
    Based on commit: https://github.com/SerenityS/android_device_samsung_slte/commit/291ab32c55c0676bf242496a5908350a44233e38

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,11 +86,35 @@ USE_OPENGL_RENDERER := true
 # Use Exynos BGRA mixer
 BOARD_USE_BGRA_8888 := true
 
++### SURFACEFLINGER
 # frameworks/native/services/surfaceflinger
 # Android keeps 2 surface buffers at all time in case the hwcomposer
 # misses the time to swap buffers (in cases where it takes 16ms or
 # less). Use 5 to avoid timing issues.
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 5
+# frameworks/native/services/surfaceflinger
+# Set the phase offset of the system's vsync event relative to the hardware
+# vsync. The system's vsync event drives Choreographer and SurfaceFlinger's
+# rendering. This value is the number of nanoseconds after the hardware vsync
+# that the system vsync event will occur.
+#
+# This phase offset allows adjustment of the minimum latency from application
+# wake-up (by Choregographer) time to the time at which the resulting window
+# image is displayed.  This value may be either positive (after the HW vsync)
+# or negative (before the HW vsync).  Setting it to 0 will result in a
+# minimum latency of two vsync periods because the app and SurfaceFlinger
+# will run just after the HW vsync.  Setting it to a positive number will
+# result in the minimum latency being:
+#
+#     (2 * VSYNC_PERIOD - (vsyncPhaseOffsetNs % VSYNC_PERIOD))
+#
+# Note that reducing this latency makes it more likely for the applications
+# to not have their window content image ready in time.  When this happens
+# the latency will end up being an additional vsync period, and animations
+# will hiccup.  Therefore, this latency should be tuned somewhat
+# conservatively (or at least with awareness of the trade-off being made).
+VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
+SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
