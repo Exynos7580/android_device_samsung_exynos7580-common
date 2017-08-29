a31c0a3652052e5ca138217bf1c562f0454cbf72
Andreas Schneider <asn@cryptomilk.org>
2017-08-29 12:18:59 +0200

    board: Use CM charger

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -223,6 +223,7 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_HAS_DOWNLOAD_MODE := true
 
 # Charger/Healthd
+WITH_CM_CHARGER := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_SHOW_PERCENTAGE := true
--- device-common.mk
+++ device-common.mk
@@ -54,7 +54,7 @@ PRODUCT_PACKAGES += \
 # Use cm images if available, aosp ones otherwise
 PRODUCT_PACKAGES += \
     charger_res_images \
-    cm_charger_res_images
+    charger
 
 # Camera
 PRODUCT_PACKAGES += \
