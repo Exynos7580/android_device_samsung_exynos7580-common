6684044e3d325f1305ba933033c80d61634a988b
Victor Lourme <contact@zeroside.co>
2017-09-03 19:50:48 +0200

    Revert "board: don't use surface buffers anymore" * Using no buffers offer best battery to the detriment of performances * Using 3 buffer offer a good perfs/battery ratio * Using 5 buffers offer very good perfs to the detriment of battery
    Using 3 buffers so.

--- board/surface.mk
+++ board/surface.mk
@@ -4,8 +4,8 @@
 
 # Surface Buffers
 # Android stock uses 2 buffers, increasing buffers will drain more battery but optimizing (touch)screen performances
-# NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
 # VSYNC
-# VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
-# SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
+VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
+SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
