1ed3ac1cabb776b6612e2ce29697401d6e065182
Stricted <info@stricted.net>
2017-12-31 23:37:39 +0100

    update sepolicy
    Change-Id: I8b78b35962f9e767d8dadbd480d6018245cf4a2f

--- Android.mk
+++ Android.mk
@@ -25,14 +25,10 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 # Create links for OpenCL files
 $(shell mkdir -p  $(TARGET_OUT)/vendor/lib; \
 	mkdir -p  $(TARGET_OUT)/vendor/lib/hw; \
-    ln -sf /vendor/lib/egl/libGLES_mali.so \
-    $(TARGET_OUT)/vendor/lib/libOpenCL.so.1.1; \
-    ln -sf /system/vendor/lib/libOpenCL.so.1.1 \
-    $(TARGET_OUT)/vendor/lib/libOpenCL.so.1; \
-    ln -sf /system/vendor/lib/libOpenCL.so.1 \
-    $(TARGET_OUT)/vendor/lib/libOpenCL.so; \
-    ln -sf /system/vendor/lib/egl/libGLES_mali.so \
-    $(TARGET_OUT)/vendor/lib/hw/vulkan.exynos5.so)
+    ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT)/vendor/lib/libOpenCL.so.1.1; \
+    ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT)/vendor/lib/libOpenCL.so.1; \
+    ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT)/vendor/lib/libOpenCL.so; \
+    ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT)/vendor/lib/hw/vulkan.exynos5.so)
 
 ifneq ($(FORCE_32_BIT),true)
 $(shell mkdir -p  $(TARGET_OUT)/vendor/lib64; \
--- board/includes.mk
+++ board/includes.mk
@@ -16,7 +16,7 @@ TARGET_SYSTEM_PROP := $(VENDOR_PATH)/system.prop
 BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
 
 # SELinux
-# BOARD_SEPOLICY_DIRS := $(VENDOR_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS := $(VENDOR_PATH)/sepolicy
 
 # Seccomp filters
 BOARD_SECCOMP_POLICY += $(VENDOR_PATH)/seccomp
