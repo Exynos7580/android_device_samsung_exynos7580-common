2ed66d8ffab326bc902c19c35dc82dfab4b3c233
Stricted <info@stricted.net>
2018-01-03 04:46:44 +0100

    update hardware/Android.mk
    Change-Id: Ia70cdf441ee47c55a3cc9c6e241a0e3f063f864c

--- hardware/Android.mk
+++ hardware/Android.mk
@@ -16,15 +16,15 @@
 ifeq ($(TARGET_RIL_VARIANT),exynos7580-common)
 
 # Include OSS Samsung Hardware-Projects (except ril)
-include $(SAM_ROOT)/AdvancedDisplay/Android.mk
+# include $(SAM_ROOT)/AdvancedDisplay/Android.mk
 include $(SAM_ROOT)/audio/Android.mk
 include $(SAM_ROOT)/consumerir/Android.mk
-include $(SAM_ROOT)/dtbhtool/Android.mk
+# include $(SAM_ROOT)/dtbhtool/Android.mk
 include $(SAM_ROOT)/fingerprint/Android.mk
 include $(SAM_ROOT)/liblights/Android.mk
 include $(SAM_ROOT)/modemloader/Android.mk
 include $(SAM_ROOT)/power/Android.mk
-
+# include $(SAM_ROOT)/ril/Android.mk
 # Include RIL-projects (except libril)
 # Don't include libsecril-client as we use own
 # include $(SAM_ROOT)/ril/libsecril-client/Android.mk
