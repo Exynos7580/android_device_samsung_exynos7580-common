1d06968949135f5b423fe95866cfd59f09860a5f
Victor Lourme <victorlourme@vlour.me>
2018-01-07 12:59:30 +0100

    exynos7580: graph/mem: remove libion and libfimg * libfimg isn't needed * libion need to be stock on our device for Oreo
    Change-Id: I760500aa9ed2fe36245832875f53f0bcd87ae6ad

--- product/graphics.mk
+++ product/graphics.mk
@@ -1,10 +1,8 @@
 ####################
-# Graphics         #
+# Graphics
 ####################
 
 PRODUCT_PACKAGES += \
-	libion \
-	libfimg \
 	android.hardware.graphics.allocator@2.0-impl \
 	android.hardware.graphics.allocator@2.0-service \
 	android.hardware.graphics.composer@2.1-impl \
