365990f7494df09a3a7ec0fc1fc70718044063cf
Victor Lourme <victorlourme@vlour.me>
2018-01-18 23:27:04 +0100

    exynos7580: camera: remove -service hidl * fc camera
    Change-Id: I2ab06ba0887ee282e10d300ad1aec4a4579de2b9

--- product/camera.mk
+++ product/camera.mk
@@ -6,5 +6,4 @@ PRODUCT_PACKAGES += \
 	Snap \
 	camera.device@3.2-impl \
 	camera.device@1.0-impl \
-	android.hardware.camera.provider@2.4-impl \
-	android.hardware.camera.provider@2.4-service
+	android.hardware.camera.provider@2.4-impl
