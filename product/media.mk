e73b17d91d46673b19d04b197f3b06969ad787cb
Stricted <info@stricted.net>
2018-02-24 22:38:07 +0100

    add missing init files
    Change-Id: Ia5fd0e2478384c294acf4fbbc9681ef59bd8510c

--- product/media.mk
+++ product/media.mk
@@ -8,3 +8,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
+# Overriden service definition
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/init/android.hardware.media.omx@1.0-service.rc:system/vendor/etc/init/android.hardware.media.omx@1.0-service.rc \
+    $(LOCAL_PATH)/configs/init/mediaserver.rc:system/etc/init/mediaserver.rc
