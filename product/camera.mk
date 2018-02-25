3ea14628420cb66d7e3975da4b3e1f0ea2b497fb
Stricted <info@stricted.net>
2018-02-25 14:50:53 +0100

    use legacy camera device implementation
    Change-Id: If3eeebfc36a3693b1dd1c282a03b854e81b9da5b

--- product/camera.mk
+++ product/camera.mk
@@ -5,5 +5,5 @@
 PRODUCT_PACKAGES += \
 	Snap \
 	camera.device@3.2-impl \
-	camera.device@1.0-impl \
-	android.hardware.camera.provider@2.4-impl
+	camera.device@1.0-impl-legacy \
+	android.hardware.camera.provider@2.4-impl-legacy
