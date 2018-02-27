4a6a3e6f6909ea1d208ec2eae4ebb1e71aa0f544
Victor Lourme <l0w@protonmail.ch>
2018-02-27 18:39:45 +0100

    exynos7580: surface: use 3 buffers
    * A3 and A5 have too slow UI, testing using 3 buffers to increase the fluidity

--- board/surface.mk
+++ board/surface.mk
@@ -4,7 +4,7 @@
 
 # Surface Buffers
 # Android stock uses 2 buffers, increasing buffers will drain more battery but optimizing (touch)screen performances
-# NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
 # VSYNC
 VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
