5ef7763eaed0619f1fe10481ad1742759e7cfdf0
Stricted <info@stricted.net>
2018-03-07 01:37:24 +0100

    ramdisk fixup
    Change-Id: I97882bd03d9ff7cde335513ac9b5deab92f63e06

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -3,4 +3,11 @@
 ####################
 
 PRODUCT_PACKAGES += \
-    init.power.rc
+    init.power.rc \
+    fstab.samsungexynos7580 \
+    init.baseband.rc \
+    init.samsung.rc \
+    init.samsungexynos7580.rc \
+    init.samsungexynos7580.usb.rc \
+    init.wifi.rc \
+    ueventd.samsungexynos7580.rc
