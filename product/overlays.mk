00cd01d1a7fe5bd1036908797d94d086885d4e5f
Stricted <info@stricted.net>
2018-04-28 01:50:33 +0200

    split overlays to exlude some values from gv family
    Change-Id: I6384e88281568cd4cc857b3971eec220cd023c28

--- product/overlays.mk
+++ product/overlays.mk
@@ -2,4 +2,8 @@
 # Overlays         #
 ####################
 
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-gv
+
+ifeq ($(filter gvwifi gvlte, $(TARGET_DEVICE)),)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+endif
