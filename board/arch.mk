0ce432489e4dbdc66498dac91924e8303195b227
Stricted <info@stricted.net>
2018-08-12 22:43:08 +0200

    correct TARGET_ARCH_VARIANT to armv8-a
    Change-Id: I359ce8b7f004236495ec2b5252a3b21f855a5e44

--- board/arch.mk
+++ board/arch.mk
@@ -3,7 +3,7 @@
 ####################
 
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
