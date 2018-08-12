f4dc744fe43d24bca46b3399a03cf8a425a9b7ca
Stricted <info@stricted.net>
2018-08-12 22:52:45 +0200

    kernel: Remove ANDROID_BUILD_TOP
    This is no longer used in Pie, so just get the top dir of the build system
    and use that.
    
    Change-Id: I6d91b3c7484596b76546ccdf06fb1ea538067d82

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,11 +2,13 @@
 # Kernel		   #
 ####################
 
+BUILD_TOP := $(shell pwd)
+
 # Kernel configuration
 TARGET_LINUX_KERNEL_VERSION := 3.10
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN := $(BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 2048
