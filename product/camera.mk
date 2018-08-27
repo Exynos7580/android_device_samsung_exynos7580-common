db1240accfaa4e8c5cb5bc82ca96d948e01992a3
Stricted <info@stricted.net>
2018-08-27 10:17:29 +0200

    correct camera HIDL interfaces
    Change-Id: I33e9392fe168b11c12501e4d7e5d5ee76def7c2c

--- product/camera.mk
+++ product/camera.mk
@@ -10,6 +10,5 @@ PRODUCT_PACKAGES += \
 endif
 
 PRODUCT_PACKAGES += \
-	camera.device@3.2-impl \
-	camera.device@1.0-impl-legacy \
-	android.hardware.camera.provider@2.4-impl-legacy
+    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-service
